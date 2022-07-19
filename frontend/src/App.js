3dc11df75e9a1402dd036e3982dc5beb78443266
Ivan Jorgovanovic <jorgovanovicivan@gmail.com>
2022-07-20 00:19:42 +0200

    Create App

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+
+
+function App() {
+  return (
+    <div >
+      <header >
+        <a href="/">moonwatch</a>
+      </header>
+      <main>
+        List products
+      </main>
+    </div>
+  );
+}
+
+export default App;
