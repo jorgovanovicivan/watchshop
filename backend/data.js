8668d59743a81fe7801ed12f5c47bb47f45341a6
Ivan Jorgovanovic <jorgovanovicivan@gmail.com>
2022-07-20 12:14:33 +0200

    second commit server and backend

---
+++ backend/data.js
@@ -0,0 +1,50 @@
+const data={
+ products:[
+{
+
+name:'Mission to Mercury',
+slug:'omega-mercur-watch',
+category:'Watches',
+image:'/images/p3.jpg',
+price:120,
+countInStock:20,
+brand:'Omega',
+rating:4.5,
+numReviews:10,
+description:'A tribute to the fast-moving winged messenger in deep grey.',
+
+},
+{
+
+    name:'Mission on Earth',
+    slug:'omega-earth-watch',
+    category:'Watchs',
+    image:'/images/p2.jpg',
+    price:120,
+    countInStock:20,
+    brand:'Omega',
+    rating:4.5,
+    numReviews:10,
+    description:'A celebration of our precious planet in blue and green.',
+    
+    },
+
+    {
+
+        name:'Mission to the Moon',
+        slug:'omega-moon-watch',
+        category:'Watches',
+        image:'/images/p1.jpg',
+        price:120,
+        countInStock:20,
+        brand:'Omega',
+        rating:4.5,
+        numReviews:10,
+        description:'Follows the exact design and dimensions of the Omega Speedmaster Moonwatch.',
+        
+        },
+
+    ],
+};
+
+export default data;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,26 @@
 
 
+import {BrowserRouter, Link, Route, Routes}  from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 function App() {
   return (
+     <BrowserRouter>
     <div >
       <header >
-        <a href="/">moonwatch</a>
+        <Link to="/">moonwatch</Link>
       </header>
       <main>
-        List products
+        <Routes>
+
+          <Route path="/product/:slug"element={<ProductScreen/>}/>
+        <Route path="/" element={<HomeScreen/>}/>
+
+        </Routes>
+       
       </main>
     </div>
+    </BrowserRouter>
   );
 }
 
